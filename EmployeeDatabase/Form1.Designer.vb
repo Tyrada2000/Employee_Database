307fc20ebefad640dd20697221bee59ad9ff3852
radat19 <83517094+radat19@users.noreply.github.com>
2022-04-18 02:21:38 -0400

    Login

--- EmployeeDatabase/Form1.Designer.vb
+++ EmployeeDatabase/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,260 +20,91 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.txtFirstName = New System.Windows.Forms.TextBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.buttonReset = New System.Windows.Forms.Button()
-        Me.buttonRemove = New System.Windows.Forms.Button()
-        Me.buttonExit = New System.Windows.Forms.Button()
-        Me.buttonOpen = New System.Windows.Forms.Button()
-        Me.buttonInsert = New System.Windows.Forms.Button()
-        Me.buttonUpdate = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.txtJobTitle = New System.Windows.Forms.TextBox()
-        Me.txtLastName = New System.Windows.Forms.TextBox()
-        Me.txtDoH = New System.Windows.Forms.TextBox()
-        Me.lblDate = New System.Windows.Forms.Label()
-        Me.Label11 = New System.Windows.Forms.Label()
-        Me.Label12 = New System.Windows.Forms.Label()
-        Me.txtSearch = New System.Windows.Forms.TextBox()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TextBox_UN = New System.Windows.Forms.TextBox()
+        Me.TextBox_Pass = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'txtFirstName
+        'Button1
         '
-        Me.txtFirstName.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtFirstName.Location = New System.Drawing.Point(242, 201)
-        Me.txtFirstName.Name = "txtFirstName"
-        Me.txtFirstName.Size = New System.Drawing.Size(298, 45)
-        Me.txtFirstName.TabIndex = 0
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(348, 52)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Check Connection"
+        Me.Button1.UseVisualStyleBackColor = True
         '
-        'DataGridView1
+        'Button2
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(556, 100)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowHeadersWidth = 51
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(1060, 677)
-        Me.DataGridView1.TabIndex = 2
+        Me.Button2.Location = New System.Drawing.Point(260, 223)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(153, 47)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Login"
+        Me.Button2.UseVisualStyleBackColor = True
         '
-        'buttonReset
+        'Label1
         '
-        Me.buttonReset.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.buttonReset.Location = New System.Drawing.Point(21, 572)
-        Me.buttonReset.Name = "buttonReset"
-        Me.buttonReset.Size = New System.Drawing.Size(167, 61)
-        Me.buttonReset.TabIndex = 4
-        Me.buttonReset.Text = "Reset"
-        Me.buttonReset.UseVisualStyleBackColor = True
-        '
-        'buttonRemove
-        '
-        Me.buttonRemove.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.buttonRemove.Location = New System.Drawing.Point(194, 572)
-        Me.buttonRemove.Name = "buttonRemove"
-        Me.buttonRemove.Size = New System.Drawing.Size(167, 61)
-        Me.buttonRemove.TabIndex = 5
-        Me.buttonRemove.Text = "Remove"
-        Me.buttonRemove.UseVisualStyleBackColor = True
-        '
-        'buttonExit
-        '
-        Me.buttonExit.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.buttonExit.Location = New System.Drawing.Point(367, 572)
-        Me.buttonExit.Name = "buttonExit"
-        Me.buttonExit.Size = New System.Drawing.Size(167, 61)
-        Me.buttonExit.TabIndex = 6
-        Me.buttonExit.Text = "Exit"
-        Me.buttonExit.UseVisualStyleBackColor = True
-        '
-        'buttonOpen
-        '
-        Me.buttonOpen.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.buttonOpen.Location = New System.Drawing.Point(21, 493)
-        Me.buttonOpen.Name = "buttonOpen"
-        Me.buttonOpen.Size = New System.Drawing.Size(167, 61)
-        Me.buttonOpen.TabIndex = 7
-        Me.buttonOpen.Text = "Open"
-        Me.buttonOpen.UseVisualStyleBackColor = True
-        '
-        'buttonInsert
-        '
-        Me.buttonInsert.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.buttonInsert.Location = New System.Drawing.Point(194, 493)
-        Me.buttonInsert.Name = "buttonInsert"
-        Me.buttonInsert.Size = New System.Drawing.Size(167, 61)
-        Me.buttonInsert.TabIndex = 8
-        Me.buttonInsert.Text = "Insert"
-        Me.buttonInsert.UseVisualStyleBackColor = True
-        '
-        'buttonUpdate
-        '
-        Me.buttonUpdate.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.buttonUpdate.Location = New System.Drawing.Point(367, 493)
-        Me.buttonUpdate.Name = "buttonUpdate"
-        Me.buttonUpdate.Size = New System.Drawing.Size(167, 61)
-        Me.buttonUpdate.TabIndex = 9
-        Me.buttonUpdate.Text = "Update"
-        Me.buttonUpdate.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(62, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(172, 36)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "User Name"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(12, 201)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(62, 167)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(198, 46)
-        Me.Label2.TabIndex = 10
-        Me.Label2.Text = "First Name"
+        Me.Label2.Size = New System.Drawing.Size(155, 36)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
         '
-        'Label3
+        'TextBox_UN
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(12, 252)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(193, 46)
-        Me.Label3.TabIndex = 11
-        Me.Label3.Text = "Last Name"
+        Me.TextBox_UN.Location = New System.Drawing.Point(256, 126)
+        Me.TextBox_UN.Name = "TextBox_UN"
+        Me.TextBox_UN.Size = New System.Drawing.Size(175, 22)
+        Me.TextBox_UN.TabIndex = 4
         '
-        'Label4
+        'TextBox_Pass
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(12, 303)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(156, 46)
-        Me.Label4.TabIndex = 12
-        Me.Label4.Text = "Job Title"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(12, 354)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(215, 46)
-        Me.Label5.TabIndex = 13
-        Me.Label5.Text = "Date of Hire"
-        '
-        'txtJobTitle
-        '
-        Me.txtJobTitle.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtJobTitle.Location = New System.Drawing.Point(243, 303)
-        Me.txtJobTitle.Name = "txtJobTitle"
-        Me.txtJobTitle.Size = New System.Drawing.Size(298, 45)
-        Me.txtJobTitle.TabIndex = 18
-        '
-        'txtLastName
-        '
-        Me.txtLastName.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtLastName.Location = New System.Drawing.Point(243, 252)
-        Me.txtLastName.Name = "txtLastName"
-        Me.txtLastName.Size = New System.Drawing.Size(298, 45)
-        Me.txtLastName.TabIndex = 19
-        '
-        'txtDoH
-        '
-        Me.txtDoH.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtDoH.Location = New System.Drawing.Point(243, 354)
-        Me.txtDoH.Name = "txtDoH"
-        Me.txtDoH.Size = New System.Drawing.Size(298, 45)
-        Me.txtDoH.TabIndex = 21
-        '
-        'lblDate
-        '
-        Me.lblDate.AutoSize = True
-        Me.lblDate.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblDate.Location = New System.Drawing.Point(23, 45)
-        Me.lblDate.Name = "lblDate"
-        Me.lblDate.Size = New System.Drawing.Size(95, 46)
-        Me.lblDate.TabIndex = 25
-        Me.lblDate.Text = "Date"
-        '
-        'Label11
-        '
-        Me.Label11.AutoSize = True
-        Me.Label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label11.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label11.Location = New System.Drawing.Point(556, 45)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(265, 48)
-        Me.Label11.TabIndex = 26
-        Me.Label11.Text = "Employee Data"
-        '
-        'Label12
-        '
-        Me.Label12.AutoSize = True
-        Me.Label12.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label12.Location = New System.Drawing.Point(1083, 48)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(127, 46)
-        Me.Label12.TabIndex = 27
-        Me.Label12.Text = "Search"
-        '
-        'txtSearch
-        '
-        Me.txtSearch.Font = New System.Drawing.Font("Times New Roman", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtSearch.Location = New System.Drawing.Point(1216, 49)
-        Me.txtSearch.Name = "txtSearch"
-        Me.txtSearch.Size = New System.Drawing.Size(400, 45)
-        Me.txtSearch.TabIndex = 28
+        Me.TextBox_Pass.Location = New System.Drawing.Point(256, 180)
+        Me.TextBox_Pass.Name = "TextBox_Pass"
+        Me.TextBox_Pass.Size = New System.Drawing.Size(175, 22)
+        Me.TextBox_Pass.TabIndex = 5
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1617, 809)
-        Me.Controls.Add(Me.txtSearch)
-        Me.Controls.Add(Me.Label12)
-        Me.Controls.Add(Me.Label11)
-        Me.Controls.Add(Me.lblDate)
-        Me.Controls.Add(Me.txtDoH)
-        Me.Controls.Add(Me.txtLastName)
-        Me.Controls.Add(Me.txtJobTitle)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
+        Me.ClientSize = New System.Drawing.Size(593, 291)
+        Me.Controls.Add(Me.TextBox_Pass)
+        Me.Controls.Add(Me.TextBox_UN)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.buttonUpdate)
-        Me.Controls.Add(Me.buttonInsert)
-        Me.Controls.Add(Me.buttonOpen)
-        Me.Controls.Add(Me.buttonExit)
-        Me.Controls.Add(Me.buttonRemove)
-        Me.Controls.Add(Me.buttonReset)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents txtFirstName As TextBox
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents buttonReset As Button
-    Friend WithEvents buttonRemove As Button
-    Friend WithEvents buttonExit As Button
-    Friend WithEvents buttonOpen As Button
-    Friend WithEvents buttonInsert As Button
-    Friend WithEvents buttonUpdate As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents txtJobTitle As TextBox
-    Friend WithEvents txtLastName As TextBox
-    Friend WithEvents txtDoH As TextBox
-    Friend WithEvents lblDate As Label
-    Friend WithEvents Label11 As Label
-    Friend WithEvents Label12 As Label
-    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents TextBox_UN As TextBox
+    Friend WithEvents TextBox_Pass As TextBox
 End Class
--- EmployeeDatabase/Form1.vb
+++ EmployeeDatabase/Form1.vb
@@ -4,146 +4,56 @@ Public Class Form1
 
     Dim sqlConn As New MySqlConnection
     Dim sqlCmd As New MySqlCommand
-    Dim sqlRd As MySqlDataReader
     Dim sqlDt As New DataTable
     Dim sqlQuery As String
     Dim DtA As New MySqlDataAdapter
-
-    Dim server As String = "localhost"
-    Dim username As String = "root"
-    Dim password As String = ""
-    Dim database As String = "employeedata"
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub updateTable()
-        sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" _
-        + "password=" + password + ";" + "database" + database
-
-        sqlConn.Open()
-
-        sqlCmd.CommandText = "SELECT * From employeedata.employee"
-
-        sqlRd = sqlCmd.ExecuteReader
-        sqlDt.Load(sqlRd)
-        sqlRd.Close()
-        sqlConn.Close()
-        DataGridView1.DataSource = sqlDt
-
-    End Sub
-    Private Sub buttonOpen_Click(sender As Object, e As EventArgs) Handles buttonOpen.Click
-        updateTable()
-    End Sub
-
-    Private Sub buttonInsert_Click(sender As Object, e As EventArgs) Handles buttonInsert.Click
-        sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" _
-            + "password=" + password + ";" + "database" + database
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sqlConn = New MySqlConnection
+        sqlConn.ConnectionString =
+            "server=localhost;userid=root;password=Dragons1@;database=employeedata"
         Try
             sqlConn.Open()
-            sqlQuery = "insert into employeedata.employee (Firstname, Lastname, JobTitle, DateOfHire)
-values('" & txtFirstName.Text & "','" & txtLastName.Text & "','" & txtJobTitle.Text & "','" & txtDoH.Text & "')"
-
-            sqlCmd = New MySqlCommand(sqlQuery, sqlConn)
-            sqlRd = sqlCmd.ExecuteReader
+            MessageBox.Show("Connection Successful")
             sqlConn.Close()
-        Catch ex As Exception
-            MessageBox.Show(ex.Message, "Employee System", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
         Finally
             sqlConn.Dispose()
         End Try
-        updateTable()
+
     End Sub
 
-    Private Sub buttonUpdate_Click(sender As Object, e As EventArgs) Handles buttonUpdate.Click
-        sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" _
-            + "password=" + password + ";" + "database" + database
-        sqlConn.Open()
-
-        Dim SqlCmd As New MySqlCommand
-        SqlCmd.Connection = sqlConn
-
-        With SqlCmd
-            .CommandText = "Update employeedata.employee Firstname=@Firstname, Lastname=@Lastname,
-            JobTitle=@JobTitle, DateOfHire=@DateOfHire"
-
-            .CommandText = CommandType.Text
-            .Parameters.AddWithValue("@Firstname", txtFirstName.Text)
-            .Parameters.AddWithValue("@Lastname", txtLastName.Text)
-            .Parameters.AddWithValue("@JobTitle", txtJobTitle.Text)
-            .Parameters.AddWithValue("@DateOfHire", txtDoH.Text)
-
-        End With
-        SqlCmd.ExecuteNonQuery()
-        sqlConn.Close()
-        updateTable()
-    End Sub
-
-    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
-        Try
-            txtFirstName.Text = DataGridView1.SelectedRows(0).Cells(0).Value.ToString
-            txtLastName.Text = DataGridView1.SelectedRows(0).Cells(1).Value.ToString
-            txtJobTitle.Text = DataGridView1.SelectedRows(0).Cells(2).Value.ToString
-            txtDoH.Text = DataGridView1.SelectedRows(0).Cells(3).Value.ToString
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
-    End Sub
-
-    Private Sub buttonExit_Click(sender As Object, e As EventArgs) Handles buttonExit.Click
-        Try
-            If MessageBox.Show("Confirm if you want to exit", "Employee System", MessageBoxButtons.YesNo,
-                               MessageBoxIcon.Question) = DialogResult.Yes Then
-                Application.Exit()
-            End If
-        Catch ex As Exception
-            MessageBox.Show(ex.Message, "Employee System", MessageBoxButtons.OK, MessageBoxIcon.Information)
-        End Try
-    End Sub
-
-    Private Sub buttonReset_Click(sender As Object, e As EventArgs) Handles buttonReset.Click
-        Dim txt As Control
-        Try
-            For Each txt In Me.Controls
-                If TypeOf txt Is TextBox Then
-                    txt.Text = ""
-                End If
-            Next
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
-    End Sub
-
-    Private Sub txtSearch_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtSearch.KeyPress
-        Try
-            If Asc(e.KeyChar) = 13 Then
-                Dim dv As DataView
-                dv = sqlDt.DefaultView
-                dv.RowFilter = String.Format("Firstname like '%{0}%'", txtSearch.Text)
-                DataGridView1.DataSource = dv.ToTable()
-            End If
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
-    End Sub
-
-    Private Sub buttonRemove_Click(sender As Object, e As EventArgs) Handles buttonRemove.Click
-        sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" _
-            + "password=" + password + ";" + "database" + database
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        sqlConn = New MySqlConnection
+        sqlConn.ConnectionString =
+            "server=localhost;userid=root;password=Dragons1@;database=employeedata"
+        Dim sqlRd As MySqlDataReader
 
         Try
             sqlConn.Open()
-            sqlQuery = "Remove from employeedata.employee where Firstname ='" & txtFirstName.Text & "'"
-
-            sqlCmd = New MySqlCommand(sqlQuery, sqlConn)
+            Dim Query As String
+            Query = "select * from employeedata.employeedata where user_name='" & TextBox_UN.Text & "' and passwords='" & TextBox_Pass.Text & "' "
+            sqlCmd = New MySqlCommand(Query, sqlConn)
             sqlRd = sqlCmd.ExecuteReader
+            Dim count As Integer
+            count = 0
+            While sqlRd.Read
+                count = count + 1
+            End While
+
+            If count = 1 Then
+                MessageBox.Show("Username and password are correct")
+            ElseIf count > 1 Then
+                MessageBox.Show("Username and password are Duplicate")
+            Else
+                MessageBox.Show("Username and password are not correct")
+            End If
+
             sqlConn.Close()
-        Catch ex As Exception
-            MessageBox.Show(ex.Message, "Employee System", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
         Finally
             sqlConn.Dispose()
         End Try
-        updateTable()
     End Sub
 End Class
