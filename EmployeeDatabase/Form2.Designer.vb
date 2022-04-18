2f46f27aefaa2e865f8903d44a9b20df23534141
radat19 <83517094+radat19@users.noreply.github.com>
2022-04-18 03:59:34 -0400

    Update

--- EmployeeDatabase/Form2.Designer.vb
+++ EmployeeDatabase/Form2.Designer.vb
@@ -32,13 +32,16 @@ Partial Class Form2
         Me.TextBox_Last = New System.Windows.Forms.TextBox()
         Me.TextBox_Job = New System.Windows.Forms.TextBox()
         Me.TextBox_DoH = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox_Id = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(52, 141)
+        Me.Label1.Location = New System.Drawing.Point(55, 197)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(168, 36)
         Me.Label1.TabIndex = 0
@@ -58,7 +61,7 @@ Partial Class Form2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(55, 206)
+        Me.Label2.Location = New System.Drawing.Point(55, 266)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(165, 36)
         Me.Label2.TabIndex = 2
@@ -68,7 +71,7 @@ Partial Class Form2
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(55, 281)
+        Me.Label3.Location = New System.Drawing.Point(55, 321)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(137, 36)
         Me.Label3.TabIndex = 3
@@ -78,7 +81,7 @@ Partial Class Form2
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(55, 350)
+        Me.Label4.Location = New System.Drawing.Point(52, 378)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(183, 36)
         Me.Label4.TabIndex = 4
@@ -87,7 +90,7 @@ Partial Class Form2
         'Button2
         '
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(564, 12)
+        Me.Button2.Location = New System.Drawing.Point(51, 446)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(169, 60)
         Me.Button2.TabIndex = 5
@@ -96,37 +99,67 @@ Partial Class Form2
         '
         'TextBox_First
         '
-        Me.TextBox_First.Location = New System.Drawing.Point(268, 154)
+        Me.TextBox_First.Location = New System.Drawing.Point(268, 210)
         Me.TextBox_First.Name = "TextBox_First"
         Me.TextBox_First.Size = New System.Drawing.Size(130, 22)
         Me.TextBox_First.TabIndex = 6
         '
         'TextBox_Last
         '
-        Me.TextBox_Last.Location = New System.Drawing.Point(268, 219)
+        Me.TextBox_Last.Location = New System.Drawing.Point(268, 279)
         Me.TextBox_Last.Name = "TextBox_Last"
         Me.TextBox_Last.Size = New System.Drawing.Size(130, 22)
         Me.TextBox_Last.TabIndex = 7
         '
         'TextBox_Job
         '
-        Me.TextBox_Job.Location = New System.Drawing.Point(268, 294)
+        Me.TextBox_Job.Location = New System.Drawing.Point(268, 334)
         Me.TextBox_Job.Name = "TextBox_Job"
         Me.TextBox_Job.Size = New System.Drawing.Size(130, 22)
         Me.TextBox_Job.TabIndex = 8
         '
         'TextBox_DoH
         '
-        Me.TextBox_DoH.Location = New System.Drawing.Point(268, 363)
+        Me.TextBox_DoH.Location = New System.Drawing.Point(268, 391)
         Me.TextBox_DoH.Name = "TextBox_DoH"
         Me.TextBox_DoH.Size = New System.Drawing.Size(130, 22)
         Me.TextBox_DoH.TabIndex = 9
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(236, 446)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(180, 60)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Update"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(150, 139)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 36)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Id"
+        '
+        'TextBox_Id
+        '
+        Me.TextBox_Id.Location = New System.Drawing.Point(268, 139)
+        Me.TextBox_Id.Name = "TextBox_Id"
+        Me.TextBox_Id.Size = New System.Drawing.Size(130, 22)
+        Me.TextBox_Id.TabIndex = 12
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1111, 529)
+        Me.Controls.Add(Me.TextBox_Id)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.TextBox_DoH)
         Me.Controls.Add(Me.TextBox_Job)
         Me.Controls.Add(Me.TextBox_Last)
@@ -154,4 +187,7 @@ Partial Class Form2
     Friend WithEvents TextBox_Last As TextBox
     Friend WithEvents TextBox_Job As TextBox
     Friend WithEvents TextBox_DoH As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox_Id As TextBox
 End Class
--- EmployeeDatabase/Form2.vb
+++ EmployeeDatabase/Form2.vb
@@ -17,12 +17,37 @@ Public Class Form2
         Try
             sqlConn.Open()
             Dim Query As String
-            Query = "insert into employeedata.employeedata (Firstname,Lastname,JobTitle,DateOfHire) 
-values ('" & TextBox_First.Text & "','" & TextBox_Last.Text & "','" & TextBox_Job.Text & "','" & TextBox_DoH.Text & "')"
+            Query = "insert into employeedata.employeedata (EmId,Firstname,Lastname,JobTitle,DateOfHire) 
+values ('" & TextBox_Id.Text & "','" & TextBox_First.Text & "','" & TextBox_Last.Text & "','" & TextBox_Job.Text & "','" & TextBox_DoH.Text & "')"
             sqlCmd = New MySqlCommand(Query, sqlConn)
             sqlRd = sqlCmd.ExecuteReader
 
             MessageBox.Show("Data Saved")
+            sqlConn.Close()
+
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+        Finally
+            sqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        sqlConn = New MySqlConnection
+        sqlConn.ConnectionString =
+            "server=localhost;userid=root;password=Dragons1@;database=employeedata"
+        Dim sqlRd As MySqlDataReader
+
+        Try
+            sqlConn.Open()
+            Dim Query As String
+            Query = "update employeedata.employeedata 
+set EmId='" & TextBox_Id.Text & "',Firstname='" & TextBox_First.Text & "',Lastname='" & TextBox_Last.Text & "',JobTitle='" & TextBox_Job.Text & "',DateOfHire='" & TextBox_DoH.Text & "'
+where EmId='" & TextBox_Id.Text & "'"
+            sqlCmd = New MySqlCommand(Query, sqlConn)
+            sqlRd = sqlCmd.ExecuteReader
+
+            MessageBox.Show("Update Successful")
             sqlConn.Close()
 
         Catch ex As MySqlException
